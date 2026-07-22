c2288a9f0a99b1d2ea82ba83a1872566ea30d9ba
vova <vladimirrusl212@gmail.com>
2026-07-22 18:27:01 +0300

    .

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ from kivy import platform
 from kivy.clock import Clock
 from kivy.metrics import sp, dp
 from random import randint
+from kivy.core.window import Keyboard
 DIR_UP=1
 DIR_DOWN=-1
 FPS=60
 SPEED=dp(5)
+BULLET_SPEED=dp(10)
 class MainScreen(MDScreen):
     ...
 
@@ -29,6 +31,12 @@ class Ship(Image):
 
     def move_right(self):
         self.pos[0]+=SPEED
+    def shot(self):
+        bullet=Bullet(self.direction,owner=self)
+        bullet.center_x=self.center_x
+        bullet.y=self.top if self.direction==DIR_UP else self.y-bullet.height
+        self.parent.add_widget(bullet)
+        self.parent.parent.parent.parent.bullets.append(bullet)
 
 
 class PlayerShip(Ship):
@@ -39,6 +47,9 @@ class PlayerShip(Ship):
                     self.move_left()
                 elif key=="right"and self.center_x < Window.width:
                     self.move_right()
+                elif key == "shot":
+                    self.shot()
+                    keys[key]=False
 
 
 class EnemyShip(Ship):
@@ -50,14 +61,33 @@ class EnemyShip(Ship):
         self.pos[1]-=dp(3)
         self.frame+=1
 
+
+class Bullet(Image):
+    def __init__(self,direction,owner, **kwargs):
+        super().__init__(**kwargs)
+        self.direction=direction
+        self.owner = owner
+
 class GameScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.ship=self.ids.ship
         self.eventkeys={}
         self.enemy_ship=[]
+        self.bullets=[]
         self.time_last_spawn=0
         self.delay_spawn=2
+
+
+        Window.bind(on_key_down=self._on_key_down, on_key_up=self._on_key_up)
+        # Керування з клавіатури під час тестування з комп'ютера
+    def _on_key_down(self, window, keycode, *args, **kwargs):
+        key = key if (key := Keyboard.keycode_to_string(window, keycode)) != 'spacebar' else 'shot'
+        self.eventkeys[key] = True
+
+    def _on_key_up(self, window, keycode, *args, **kwargs):
+        key = key if (key := Keyboard.keycode_to_string(window, keycode)) != 'spacebar' else 'shot'
+        self.eventkeys[key] = False
 
     def spawn_enemy(self):
         enemy=EnemyShip()
@@ -81,6 +111,14 @@ class GameScreen(MDScreen):
         if self.time_last_spawn>self.delay_spawn:
             self.spawn_enemy()
             self.time_last_spawn=0
+
+        for bullet in self.bullets[:]:
+            bullet.y += BULLET_SPEED* bullet.direction
+            self.check_collision(bullet)
+            if bullet .top<0 or bullet.y>Window.height:
+
+                self.bullets.remove(bullet)
+                self.ids.front.remove_widget(bullet)
     
         for enemy in self.enemy_ship:
             enemy.update()
@@ -89,6 +127,21 @@ class GameScreen(MDScreen):
                 self.ids.front.remove_widget(enemy)
             if enemy.collide_widget(self.ship):
                 self.game_over()
+    def check_collision(self, bullet):
+        if bullet.owner== self.ship:
+            for enemy in self.enemy_ship[:]:
+                if bullet.collide_widget(enemy):
+                    self.enemy_ship.remove(enemy)
+                    self.ids.front.remove_widget(enemy)
+                    self.bullets.remove(bullet)
+                    self.ids.front.remove_widget(bullet)
+                    break
+        else:
+            if bullet.collide_widget(self.ship):
+                self.bullets.remove(bullet)
+                self.ids.front.remove_widget(bullet)
+                self.game_over
+                
 
     def game_over(self):
         self.updateEvent.cancel()
@@ -96,7 +149,7 @@ class GameScreen(MDScreen):
             self.enemy_ship.remove(enemy)
             self.ids.front.remove_widget(enemy) 
         self.manager.current='gameover'
-        
+
 class ShooterApp(MDApp):
      def build(self):
         Builder.load_file("shooter.kv")
